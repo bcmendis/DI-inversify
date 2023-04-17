ebf3048bb22455f49df3e7c49f8f47a79e36d32c
bcmendis <benclintonmendis@gmail.com>
2023-04-16 20:27:44 -0400

    test desktop

--- index.ts
+++ index.ts
@@ -1,30 +1,27 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { TodoClient, ApiManager } from './entities';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { TodoClient, ApiManager } from "./entities";
 import { myContainer } from "./inversify.config";
 import { TYPES } from "./types";
 import { ApiManagerInterface } from "./interfaces";
-
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-
 const test = myContainer.get<ApiManagerInterface>(TYPES.ApiManager);
 
-
+// test
 // expect(ninja.fight()).eql("cut!"); // true
 // expect(ninja.sneak()).eql("hit!"); // true
 
+app.get("/", async (req: Request, res: Response) => {
+  //     const client = new TodoClient();
+  //     const x=new ApiManager(client);
 
-app.get('/', async (req: Request, res: Response) => {
-//     const client = new TodoClient();
-//     const x=new ApiManager(client);
-
-//   res.send(await x.fetchData());
-await test.fetchData().then(result => console.log(result));
+  //   res.send(await x.fetchData());
+  await test.fetchData().then((result) => console.log(result));
 });
 
 app.listen(port, () => {
